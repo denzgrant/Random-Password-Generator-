8d79e7cac76e0546326e0ca387ec13967d4e731d
Denzell Grant <denzgrant@gmail.com>
2020-03-19 23:27:45 -0700

    Cleaned up code comepletely. Password generation is calculated by user's character length input

--- Develop/script.js
+++ Develop/script.js
@@ -1,82 +1,57 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseLetters = upperCaseLetters.toLowerCase();
 var specialCharacters = "!@#$%^&*()_+-";
-//List of questions regarding password preference
-var userSelect = {
-  numOfCharacters: function () {
-    numOfCharAnswer = prompt("How many characters do you want?");
-  },
-  upperChar: function () {
-    upperCharAnswer = confirm("Do you want to use an uppercase letter?");
-    if (upperCharAnswer === true) {
-      var result = "";
-      result += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
-    } else {
-      alert("No uppercase letter has been selected");
-    };
-    return result;
-  },
-
-  specialChar: function () {
-    specialCharAnswer = confirm("Do you want to use an special character?");
-    if (upperCharAnswer === true) {
-      var result = "";
-      result += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-    } else {
-      alert("No special character has been selected");
-    };
-    return result;
-  },
-
-  lowerChar: function () {
-    lowerCharAnswer = confirm("Do you want to use a lowercase letter?");
-    if (lowerCharAnswer === true) {
-      var result = "";
-      result += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-    } else {
-      alert("No lowercase letter has been selected");
-    };
-    return result;
-  },
-  numChar: function () {
-    numCharAnswer = confirm("Do you want to use a number?");
-    if (numCharAnswer === true) {
-      var result = "";
-      result += Math.floor(Math.random() * 127) + 1;
-    } else {
-      alert("No number has been selected");
-    };
-    return result;
-  }
-};
-// Write password to the #password input
-function writePassword() {
-  generateBtn.addEventListener("click", createPassword);
-  // var passwordText = document.querySelector("#password");
-  // passwordText.value = password;
-  var upperCharResult = userSelect.upperChar();
-  var specialCharResult = userSelect.specialChar();
-  var lowerCharResult = userSelect.lowerChar();
-  var numCharResult = userSelect.numChar();
-  const userResult = upperCharResult + specialCharResult + lowerCharResult + numCharResult;
-  console.log(userResult);
-  document.getElementById("password").placeholder = userResult;
-}
-////look up document object add text and button click
-// Add event listener to generate button
-
+var numbers = "012345689";
+var allChar = upperCaseLetters + lowerCaseLetters + specialCharacters + numbers;
+var NoCaps = lowerCaseLetters + specialCharacters + numbers;
 //User is going to be questioned regarding password preference
 function createPassword() {
-  numOfCharAnswer = prompt("How many characters do you want?\nHas to be a number between 8 and 128");
-  while (numOfCharAnswer > 128 || numOfCharAnswer < 8) {
-    alert("Has to be a number between 8 to 128");
-    numOfCharAnswer = prompt("How many characters do you want?");
-    //User won't be able to continue if they select an option less than 8 or more than 128 
+
+  upperChar();
+}
+//List of questions regarding password preference
+function upperChar() {
+  upperCharAnswer = confirm("Do you want to use an uppercase letter?");
+  if (upperCharAnswer === false) {
+    alert("No Upper Character");
   }
-  alert("Thank you");
-  writePassword();
+  specialChar();
 }
 
+function specialChar() {
+  specialCharAnswer = confirm("Do you want to use an special character?");
+  lowerChar();
+}
 
+function lowerChar() {
+  lowerCharAnswer = confirm("Do you want to use a lowercase letter?");
+  numChar();
+}
+
+function numChar() {
+  numCharAnswer = confirm("Do you want to use a number?");
+  passwordSelection = createPassword + numChar + lowerChar + specialChar + upperChar;
+  if (passwordSelection === "") {
+    alert("You have to select an option");
+  }
+  function getRandomPass(number) {
+    var result = "";
+    for (var i = 0; i < number; i++) {
+      result += allChar.charAt(Math.floor(Math.random() * allChar.length));
+
+    }
+    console.log(result);
+    document.querySelector("#password").placeholder = result;
+  }
+
+  var numOfCharAnswer = Number(prompt("How many characters do you want?"));
+  while (numOfCharAnswer > 128 || numOfCharAnswer < 8) {
+    alert("Has to be a number between 8 to 128");
+    numOfCharAnswer = Number(prompt("How many characters do you want?"));
+    //User won't be able to continue if they select an option less than 8 or more than 128 
+  }
+  getRandomPass(numOfCharAnswer);
+
+}
+// document.querySelector("#password").placeholder = result;
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Random-Password-Generator-
 Random Password Generator that uses numbers, letters, and special characters. 
+
+User has the ability to customize their password by selecting if they want to use numbsers, uppercase or lowercase letters, and special characters.
+
+A random selection of the chosen characters will appear by a click of a button. 
